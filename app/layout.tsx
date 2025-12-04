b157b6f1db4a13103a8eab8abbf41369d9694c87
TheAhsanFarabi <ahsanfarabi1971@gmail.com>
2025-12-04 21:43:46 +0600

    Layout Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className="bg-zinc-50 dark:bg-black">
       <body className="font-sans min-h-screen flex flex-col">
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-2">{children}</main>
         <Footer />
       </body>
     </html>
